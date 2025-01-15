c41ec597417cba4576d32c7f6c87623a58db3ed3
JENNIFER974-DEL <jennifernyaguthii23@gmail.com>
2025-01-15 15:46:53 +0300

    added notes

---
+++ javascript.js
@@ -0,0 +1,45 @@
+// var myName = "jenny";
+// let mySecondName = "nyaguthii";
+// console.log(myName.toUpperCase());
+// console.log(mySecondName.length);
+
+// let username ="victor";
+// let password = 2345677;
+// if (username ==="victor" && password ===2345677) {
+//     console.log("welcome" + username);
+// } else {
+//     console.log("wrong username or password");
+// }
+// let MyName ="Jennifer";
+// let Hobby ="My hobby is listening to podcasts";
+
+// function elaborateHobby () {
+//     let HobbyDetails ="I enjoy listening to podcasts about mental health, spirituality, technology, and finances. \n My other hobby is yoga";   
+//      let AboutMe ="I am a mental health advocate, passionate about spirituality, technology, and minimalism";
+
+//     console.log('MyName: Jennifer');
+//     console.log('Hobby: My hobby is listening to podcast');
+//     console.log('HobbyDetails: I enjoy listening to podcasts about mental health, spirituality, technology, and finances. \n My other hobby is yoga');
+//     console.log('AboutMe: I am a mental health advocate, passionate about spirituality, technology, and minimalism');
+
+// if (Hobby = "My hobby is listening to podcasts") {
+//     let HobbyBenefits ="Podcasts open my mind to new ways of thinking and i acquire alot of knowledge on the go";
+//     console.log('HobbyBenefits: Podcasts open my mind to new ways of thinking and i acquire alot of knowledge on the go');  
+// }
+// }
+
+// elaborateHobby();
+
+// 
+let breakfast = ["chapati", "sausages", "matoke"];
+breakfast.push("chapati", "sausages", "matoke");
+// breakfast.pop();
+// breakfast.unshift("tea");
+// console.log(breakfast);
+// breakfast.shift();
+// console.log(breakfast);
+breakfast.splice(1, 1);
+console.log(breakfast);
+
+
+
---
+++ splice.js
@@ -0,0 +1,6 @@
+let breakfast = ["chapati", "sausages", "matoke"];
+console.log(breakfast);
+// breakfast.splice(breakfast.length - 1, 2);
+// console.log(breakfast);
+let breakfastcopy = breakfast.slice(1, -1);
+console.log(breakfastcopy);
